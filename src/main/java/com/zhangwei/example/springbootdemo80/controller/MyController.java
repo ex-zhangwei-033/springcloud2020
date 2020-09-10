b7a0a05c153f87584e6066f8b2e31f788d465dbe
ex-zhangwei-033 <823219314@qq.com>
2020-09-10 22:47:14 +0800

    second commit

--- src/main/java/com/zhangwei/example/springbootdemo80/controller/MyController.java
+++ src/main/java/com/zhangwei/example/springbootdemo80/controller/MyController.java
@@ -1,5 +1,6 @@
 package com.zhangwei.example.springbootdemo80.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @GetMapping("/hello")
     private String hello(){
-        return "Hello World";
+        return "Hello World  !  端口："+serverPort;
     }
 }
